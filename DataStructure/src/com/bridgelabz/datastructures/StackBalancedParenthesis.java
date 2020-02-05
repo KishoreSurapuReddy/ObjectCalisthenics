2c35444f5f384df2e6a11068ff92cad088e48f90
kishorereddy0909 <kishorereddy996@gmail.com>
2020-02-05 14:48:04 +0530

    [kishorereddy] Refactor. added balancedStack function to StackBalancedParenthesis.java in com.bridgelabz.datastructures

--- DataStructure/src/com/bridgelabz/datastructures/StackBalancedParenthesis.java
+++ DataStructure/src/com/bridgelabz/datastructures/StackBalancedParenthesis.java
@@ -8,20 +8,45 @@
 package com.bridgelabz.datastructures;
 import com.bridgelabz.util.Stack;
 
-public class StackBalancedParenthesis {
+public class StackBalancedParenthesis<T> {
+	static Stack<Character> stack = new Stack<Character>();
+	static int openbrace=0;
+	static int closebrace=0;
+	
+	//method to check expression is balanced or not
+	public static boolean balancedStack() {
+		boolean isbalanced = false;
+		if (openbrace == closebrace) {
+			isbalanced = true;
+		}
+		return isbalanced;
+	}
 
-	public static void main(String[] args) {
-		Stack<Character> stack = new Stack<Character>();
+	public static void main(String[] args) throws Exception {
 		//given string expression
-		String expression = "(5+6)∗(7+8)/(4+3)(5+6)∗(7+8)/*(4+3))";
+		String expression = "(5+6)∗(7+8)/(4+3)(5+6)∗(7+8)/*(4+3)";
 		for (int index = 0; index < expression.length(); index++) {
 			char character = expression.charAt(index);
 			if (character == '(' || character == ')') {
 				stack.pushCharacter(character);
 			}
 		}
-		stack.popCharacter();
-		boolean result = stack.balancedStack();
+		char data = 0;
+		System.out.println(stack.sizeOfStack());
+		for (int index = 0; index < stack.sizeOfStack(); index++) {
+			char character = expression.charAt(index);
+			if (character == '(' || character == ')') {
+				data = stack.popCharacter(character);
+			}
+			if(data == '(') {
+				openbrace++;
+				System.out.print("count is:"+openbrace);
+			}else if(data == ')') {
+				closebrace++;
+				System.out.print(closebrace);
+			}
+		}
+		boolean result = balancedStack();
 		if (result == true) {
 			System.out.println("given arthemetic expression was balanced ");
 			return;
