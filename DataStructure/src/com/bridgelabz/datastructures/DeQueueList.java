373d4377eddefbb171d2b23aa6c8cf4be520f97c
kishorereddy0909 <kishorereddy996@gmail.com>
2020-02-05 13:20:37 +0530

    [kishorereddy] Refactor. added isPalindrome function to DeQueueList.java in com.bridgelabz.datastructures

--- DataStructure/src/com/bridgelabz/datastructures/DeQueueList.java
+++ DataStructure/src/com/bridgelabz/datastructures/DeQueueList.java
@@ -6,23 +6,50 @@
  */
 package com.bridgelabz.datastructures;
 
+import java.util.Scanner;
+
 import com.bridgelabz.util.DeQueue;
 
 public class DeQueueList {
+	static DeQueue<Character> dequeue = new DeQueue<Character>();
+	
+	/**funcrtion to implement given string is palindrome or not
+	 * @return boolean isPalindrome
+	 */
+	public static boolean isPalindrome() {
+		Character name1 = ' ';
+		Character name2 = ' ';
+		boolean ispalindrome = false;
+		int size = dequeue.deQueueSize();
+		if (size % 2 == 0) {
+			while (size != 0) {
+				name1 = (Character) dequeue.removeFront();
+				name2 = (Character) dequeue.removeRear();
+				size--;
+			}
+			if (name1.equals(name2)) {
+				ispalindrome = true;
+			}
+		} else if (size % 2 == 1) {
+			System.out.println("deque contains insufficient  character ");
+		}
+		return ispalindrome;
+	}
 
 	public static void main(String[] args) {
-		DeQueue<Character> dequeue = new DeQueue<Character>(); 
+	    Scanner scanner = new Scanner(System.in);
 		String name = null;
 		try {
 			System.out.println("Enter the  String :");
-			name = dequeue.scanner.next();
+			name = scanner.next();
 		} catch (Exception e) {
 			System.out.println("enter string type only..");
 		}
 		for (int index = 0; index < name.length(); index++) {
 			dequeue.addRear(name.charAt(index));
 		}
+		scanner.close();
 		System.out.println(dequeue.deQueueSize());
-		System.out.println("given string is palindrome :" + dequeue.isPalindrome());
+		System.out.println("given string is palindrome :" +isPalindrome());
 	}
 }
