c3ec3aa21cc0d12b82593efdccc2606903d58f2d
kishorereddy0909 <kishorereddy996@gmail.com>
2020-02-05 14:53:56 +0530

    [kishorereddy] Refactor. added Exceptions and generics in Queue.java in com.bridgelabz.datastructures

--- DataStructure/src/com/bridgelabz/util/Queue.java
+++ DataStructure/src/com/bridgelabz/util/Queue.java
@@ -5,21 +5,17 @@
  * @file Queue.java
 */
 package com.bridgelabz.util;
-import java.util.Scanner;
 
 public class Queue<T> {
 	Node<T> left, right;
 	int size = 0;
-	private int bankAmount = 100000;
-	public Scanner scanner = new Scanner(System.in);
 
 	/**
 	 * function to implement enQueue
 	 * @param customer
 	 */
-	@SuppressWarnings("unchecked")
-	public void enQueue(String customer) {
-		Node<String> newNode = new Node<String>(customer);
+	public void enQueue(T customer) {
+		Node<T> newNode = new Node<T>(customer);
 		if (left == null) {
 			left = right = (Node<T>) newNode;
 			return;
@@ -32,63 +28,21 @@ public class Queue<T> {
 	/**
 	 * function to implement dequeue
 	 */
-	public void deQueue() {
+	public T deQueue(){
 		@SuppressWarnings("unused")
 		Node<T> current;
 		if (left == null) {
-			System.out.println("queue is empty");
-			return;
+			throw new NullPointerException("queue is empty");
 		}
-		do {
-			while (left != null) {
-				String customername = (String) left.data;
-				System.out.println("customer name :" + customername);
-				int choice = 0;
-				int amount = 0;
-				try {
-					System.out.println("select the choice 1.WithdrawMoney 2.DepositMoney ");
-					choice = scanner.nextInt();
-				} catch (Exception e) {
-					System.out.println("Enter details correctly... ");
-				}
-				switch (choice) {
-				case 1:
-					try {
-						System.out.println("Enter the amount to withdraw :");
-						amount = scanner.nextInt();
-					} catch (Exception e) {
-						System.out.println("enter amount digits...");
-					}
-					if (amount <= 50000) {
-						System.out.println(customername + " has withdraw from account ");
-						withDrawCash(customername, amount);
-						System.out.println(customername + " withdraw amount is :" + amount);
-						System.out.println("Final bank amount is : " + bankAmount);
-						break;
-					} else {
-						System.out.println("daily withdraw amount limit is < 50000 only");
-					}
-				case 2:
-					try {
-						System.out.println("Enter the amount to depost :");
-						amount = scanner.nextInt();
-					} catch (Exception e) {
-						System.out.println("enter amount in digits...");
-					}
-					System.out.println(customername + " has deposited amount into bank ");
-					depositCash(customername, amount);
-					System.out.println(customername + " deposited amount is :" + amount);
-					System.out.println("Final bank amount is : " + bankAmount);
-					break;
-				default:
-					System.out.println("Select only range between 1 - 2");
-					break;
-				}
-				current = left;
-				left = left.next;
-				current = null;
-			}
-		} while (true);
+		T customername = null;
+		while (left != null) {
+			customername = (T) left.data;	
+			current = left;
+			left = left.next;
+			current = null;
+			return customername;
+		}
+		return customername;
 	}
 
 	/**
@@ -107,27 +61,5 @@ public class Queue<T> {
 	 */
 	public int sizeOfQueue() {
 		return size;
-	}
-
-	/**
-	 * function to implement deposit amount
-	 * @param customername
-	 * @param amount
-	 * @return total amount
-	 */
-	public int depositCash(String customername, int amount) {
-		bankAmount += amount;
-		return bankAmount;
-	}
-
-	/**
-	 * function to implement withdraw cash
-	 * @param customername
-	 * @param amount
-	 * @return total amount
-	 */
-	public int withDrawCash(String customername, int amount) {
-		bankAmount -= amount;
-		return bankAmount;
 	}
 }
