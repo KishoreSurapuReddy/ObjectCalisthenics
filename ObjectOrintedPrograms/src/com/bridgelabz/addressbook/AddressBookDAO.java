5b596cc4267e35c180e9820d411b0881fa2ae73e
kishorereddy0909 <kishorereddy996@gmail.com>
2020-02-07 14:42:44 +0530

    [kishorereddy] Refactor. comments added to AddressBookDAO.java in com.bridgelabz.addressbook

--- ObjectOrintedPrograms/src/com/bridgelabz/addressbook/AddressBookDAO.java
+++ ObjectOrintedPrograms/src/com/bridgelabz/addressbook/AddressBookDAO.java
@@ -1,3 +1,9 @@
+/**AddressBookDAO interface have abstract methods to implement
+ * @author kishorereddy
+ * @version 1.0
+ * @since 05-03-2020
+ * @file AdressBookDAO.java
+*/
 package com.bridgelabz.addressbook;
 
 import java.io.IOException;
@@ -5,6 +11,7 @@ import java.io.IOException;
 import org.json.simple.parser.ParseException;
 
 public interface AddressBookDAO {
+	//abstract methods
 	public void addPerson(Person person,String firstName) throws IOException, ParseException;
 	public void deletePerson(String firstName) throws IOException, ParseException;
 	public void editPerson(String firstName) throws IOException, ParseException;
